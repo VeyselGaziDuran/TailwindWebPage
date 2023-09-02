5db980792055da212c26bc4f1ce167a26f9f9a01
Veysel Gazi Duran <veyselduran82@gmail.com>
2023-09-02 03:40:13 +0300

    Header Component Created

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ['./public/**/*.{html,js}'],
   theme: {
-    extend: {}
+    container: {
+      center: true,
+      screens: {
+        lg: '1140px',
+        xl: '1140px',
+        '2xl': '1140px'
+      }
+    },
+    extend: {
+      fontFamily: {
+        gemunu: ['Gemunu Libre', 'sans-serif'],
+        open: ['Open Sans', 'sans-serif']
+      },
+      colors: {
+        'vage-red': '#BC1A45',
+        'vage-melon': '#FFD369',
+        'vage-grey': '#DDDDDD',
+        'vage-white': '#F7F7F7'
+      }
+    }
   },
   plugins: []
 };
